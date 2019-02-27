37811e04f913b87c810a3dac425f1bc42ba8d25d
Trevor Adcock <trevor.adcock@mac.com>
2019-02-27 14:36:15 -0700

    Finishes in Class Portion of Application

--- Azoera/CustomUI/TransparentNavigationController.swift
+++ Azoera/CustomUI/TransparentNavigationController.swift
@@ -5,5 +5,25 @@
 //  Created by DevMountain on 2/27/19.
 //  Copyright © 2019 trevorAdcock. All rights reserved.
 //
+import UIKit
 
-import Foundation
+class TransparentNavigationController: UINavigationController {
+  // MARK: - Properties
+  override var preferredStatusBarStyle: UIStatusBarStyle { // Sets the status bar style to be light
+    return .lightContent
+  }
+  
+  // MARK: - View Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupViews()
+  }
+  
+  // MARK: - Setup
+  func setupViews() {
+    navigationBar.setBackgroundImage(UIImage(), for: .default)
+    navigationBar.shadowImage = UIImage()
+    navigationBar.isTranslucent = true
+    view.backgroundColor = .clear
+  }
+}
